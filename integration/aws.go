936ca1d15d86b530d272a5c24420089203fee94d
Stephen Chan <chan.stephen.y@gmail.com>
2023-12-17 13:29:33 -0800

    integration/aws: check for s3 bucket encryption
    This addresses CC6.6 The entity implements logical access security measures to
    protect against threats from sources outside its system boundaries.

--- integration/aws.go
+++ integration/aws.go
@@ -6,26 +6,39 @@ import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
 	"github.com/aws/aws-sdk-go/service/iam"
+	"github.com/aws/aws-sdk-go/service/s3"
 )
 
 // AWS checks that the user's AWS infra is SOC2 compliant
 type AWS struct {
 	IAM *IAM
+	S3  *S3
 }
 
 // New returns a new AWS integration
 func NewAWS() (*AWS, error) {
-	s, err := session.NewSession()
+	// TODO: allow users to specify region
+	s, err := session.NewSession(aws.NewConfig().WithRegion("us-east-1"))
 	if err != nil {
 		return nil, err
 	}
 
-	return &AWS{IAM: NewIAM(s)}, nil
+	return &AWS{IAM: NewIAM(s), S3: NewS3(s)}, nil
 }
 
 // Check checks that the user's AWS infra is SOC2 compliant
 func (a *AWS) Check() ([]Result, error) {
-	return a.IAM.Check()
+	iamRes, err := a.IAM.Check()
+	if err != nil {
+		return nil, err
+	}
+
+	s3Res, err := a.S3.Check()
+	if err != nil {
+		return nil, err
+	}
+
+	return append(iamRes, s3Res...), nil
 }
 
 // IAM checks that the user's IAM infra is SOC2 compliant
@@ -136,6 +149,79 @@ func (i *IAM) userResult(user *iam.User, rule string, compliant bool, reason str
 		Resource: Resource{
 			Type: "aws/iam-user",
 			Name: aws.StringValue(user.Arn),
+		},
+		Rule:      rule,
+		Compliant: compliant,
+		Reason:    reason,
+	}
+}
+
+// S3 checks that the user's IAM infra is SOC2 compliant
+type S3 struct {
+	s3API *s3.S3
+}
+
+// NewS3 returns a new S3 integration
+func NewS3(s *session.Session) *S3 {
+	return &S3{s3API: s3.New(s)}
+}
+
+// Check checks that the user's S3 infra is SOC2 compliant
+func (s *S3) Check() ([]Result, error) {
+	return s.checkS3BucketEncryption()
+}
+
+// checkS3BucketEncryption checks that S3 buckets are encrypted
+func (s *S3) checkS3BucketEncryption() ([]Result, error) {
+	var s3Res []Result
+	rule := "S3 buckets must be encrypted"
+
+	buckets, err := s.s3API.ListBuckets(nil)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, bucket := range buckets.Buckets {
+		bucketLoc, err := s.s3API.GetBucketLocation(&s3.GetBucketLocationInput{Bucket: bucket.Name})
+		if err != nil {
+			return nil, err
+		}
+
+		region := aws.StringValue(bucketLoc.LocationConstraint)
+		if len(region) == 0 {
+			// Buckets in Region us-east-1 have a LocationConstraint of null.
+			region = "us-east-1"
+		}
+
+		regionSession := session.Must(
+			session.NewSession(
+				aws.NewConfig().WithRegion(region)))
+		regionS3API := s3.New(regionSession)
+
+		encryption, err := regionS3API.GetBucketEncryption(
+			&s3.GetBucketEncryptionInput{Bucket: bucket.Name})
+		if err != nil {
+			return nil, err
+		}
+
+		if encryption.ServerSideEncryptionConfiguration == nil {
+			s3Res = append(
+				s3Res,
+				s.bucketResult(bucket, rule, false, "Bucket is not encrypted"),
+			)
+		} else {
+			s3Res = append(s3Res, s.bucketResult(bucket, rule, true, ""))
+		}
+	}
+
+	return s3Res, nil
+}
+
+func (s *S3) bucketResult(bucket *s3.Bucket, rule string, compliant bool, reason string) Result {
+	return Result{
+		Resource: Resource{
+			Type: "aws/s3-bucket",
+			Name: aws.StringValue(bucket.Name),
 		},
 		Rule:      rule,
 		Compliant: compliant,
